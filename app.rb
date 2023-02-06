89f0da8f63c3e80c2e2969125e62bee7b7c61fc3
kenrick <kenrickdony@gmail.com>
2023-02-06 11:11:05 +0300

    passed test

--- app.rb
+++ app.rb
@@ -13,11 +13,23 @@
 # end
 # # 1 + 1
 # puts add + add
+# print "Hello world!"
+# print "Hello world!"
+# print "Hello world!"
+# puts [1,2,3].inspect
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
 
-def add num1, num2
-    puts num1
-    puts num2
-    num1 + num2
-end
+# def add num1, num2
+#     puts num1
+#     puts num2
+#     num1 + num2
+# end
 
-puts add(1,3) + add(2,2)
+# puts add(1,3) + add(2,2)
+#test 1
+puts ("Hello World!")
+
+#test 2
+print "Pass this test, please."
+# test 3
+p [1, 2, 3]
